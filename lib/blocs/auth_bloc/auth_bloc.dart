76d9ca8c10f96d83b42b5c843f4ba0cd23434fad
David Jia <105570809@qq.com>
2023-10-17 20:12:00 +0800

    implement AuthBloc

--- lib/blocs/auth_bloc/auth_bloc.dart
+++ lib/blocs/auth_bloc/auth_bloc.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:rxdart_course/blocs/auth_bloc/auth_error.dart';
 
 @immutable
 abstract class AuthStatus {
@@ -44,11 +46,113 @@ class RegisterCommand extends AuthCommand {
 }
 
 extension Loading<E> on Stream<E> {
-  Stream<E> setLoading(
+  Stream<E> setLoadingTo(
     bool isLoading, {
     required Sink<bool> onSink,
   }) =>
       doOnEach(
         (notification) => onSink.add(isLoading),
       );
+}
+
+@immutable
+class AuthBloc {
+  // Read only properties
+  final Stream<AuthStatus> authStatus;
+  final Stream<AuthError?> authError;
+  final Stream<bool> isLoading;
+  final Stream<String?> userId;
+  // Write only properties
+  final Sink<LoginCommand> login;
+  final Sink<RegisterCommand> register;
+  final Sink<void> logout;
+
+  const AuthBloc._({
+    required this.authStatus,
+    required this.authError,
+    required this.isLoading,
+    required this.userId,
+    required this.login,
+    required this.register,
+    required this.logout,
+  });
+
+  factory AuthBloc() {
+    final isLoading = BehaviorSubject<bool>();
+    final Stream<AuthStatus> authStatus = FirebaseAuth.instance
+        .authStateChanges()
+        .map((user) => user != null
+            ? const AuthStatusLoggedIn()
+            : const AuthStatusLoggedOut());
+
+    final Stream<String?> userId = FirebaseAuth.instance
+        .authStateChanges()
+        .map((user) => user?.uid)
+        .startWith(FirebaseAuth.instance.currentUser?.uid);
+
+    final login = BehaviorSubject<LoginCommand>();
+    final Stream<AuthError?> loginError = login
+        .setLoadingTo(true, onSink: isLoading)
+        .asyncMap<AuthError?>((loginCommand) async {
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: loginCommand.email,
+          password: loginCommand.password,
+        );
+        return null;
+      } on FirebaseAuthException catch (e) {
+        return AuthError.from(e);
+      } catch (_) {
+        return const AuthErrorUnknown();
+      }
+    }).setLoadingTo(false, onSink: isLoading);
+
+    final register = BehaviorSubject<RegisterCommand>();
+    final Stream<AuthError?> registerError = register
+        .setLoadingTo(true, onSink: isLoading)
+        .asyncMap<AuthError?>((registerCommand) async {
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: registerCommand.email,
+          password: registerCommand.password,
+        );
+        return null;
+      } on FirebaseAuthException catch (e) {
+        return AuthError.from(e);
+      } catch (_) {
+        return const AuthErrorUnknown();
+      }
+    }).setLoadingTo(false, onSink: isLoading);
+
+    final logout = BehaviorSubject<void>();
+    final Stream<AuthError?> logoutError = logout
+        .setLoadingTo(true, onSink: isLoading)
+        .asyncMap<AuthError?>((_) async {
+      try {
+        await FirebaseAuth.instance.signOut();
+        return null;
+      } on FirebaseAuthException catch (e) {
+        return AuthError.from(e);
+      } catch (_) {
+        return const AuthErrorUnknown();
+      }
+    }).setLoadingTo(false, onSink: isLoading);
+
+    // authError = loginError + registerError + logoutError
+    final Stream<AuthError?> authError = Rx.merge([
+      loginError,
+      registerError,
+      logoutError,
+    ]);
+
+    return AuthBloc._(
+      authStatus: authStatus,
+      authError: authError,
+      isLoading: isLoading,
+      userId: userId,
+      login: login,
+      register: register,
+      logout: logout,
+    );
+  }
 }
